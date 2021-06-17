fb746d605341f5990e42219e2a9224381930d49b
Timothy Wieser <timothyswieser@gmail.com>
2021-06-17 13:06:54 -0400

    auth routes all working correctly

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,45 @@
 const router = require('express').Router();
-
-
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
-
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 
 
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+// // GET /api/set-token-cookie
+// // http://localhost:5000/api/set-token-cookie
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         },
+//     })
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+// // GET /api/restore-user
+// //  http://localhost:5000/api/restore-user
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // GET /api/require-auth
+// //  http://localhost:5000/api/require-auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 
 module.exports = router;
