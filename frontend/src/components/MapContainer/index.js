c91329b7c7f7bd2397c2e2ee1a1646b07346c43f
tswieser <timothyswieser@gmail.com>
2021-06-26 20:53:20 -0400

    hidden api key

--- frontend/src/components/MapContainer/index.js
+++ frontend/src/components/MapContainer/index.js
@@ -31,5 +31,5 @@ export class MapContainer extends Component {
 
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyA5TiG6AMubVry0NCB8938qNNKjaAtgC_M'
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY
 })(MapContainer);
